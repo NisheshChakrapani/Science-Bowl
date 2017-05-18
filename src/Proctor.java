af2aa79d0ee92c6c138788f4b5483aed57ddcfb2
Nishesh Chakrapani <nishesh_chakrapani@outlook.com>
2017-05-18 08:12:27 -0700

    Casual mode added

--- src/Proctor.java
+++ src/Proctor.java
@@ -1,13 +1,19 @@
+import org.apache.commons.lang3.text.WordUtils;
+
 import java.io.*;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.Random;
 import java.util.Scanner;
+import java.awt.*;
 
 /**
  * Created by nishu on 3/1/2017.
  */
 public class Proctor {
     private boolean fastMode;
-    private final double OFFSET_MULTIPLIER = 53.398058252427184466019417475728;
+    private boolean visualMode;
+    private final double OFFSET_MULTIPLIER = 30;
     private boolean correct = true;
     private Stopwatch stopwatch = new Stopwatch();
     private int set;
@@ -16,22 +22,38 @@ public class Proctor {
     private final int SLOW_MODE_BONUS = 20000;
     private final int FAST_MODE_TOSS_UP = 10000;
     private final int FAST_MODE_BONUS = 25000;
+    private ArrayList<Question> unread = new ArrayList<>();
 
     public Proctor() throws IOException, InterruptedException {
-        getSetAndRound();
+        getSettings();
         readQuestions();
     }
 
-    private void getSetAndRound() {
+    private void getSettings() throws IOException {
         Scanner scan = new Scanner(System.in);
+        boolean modeFound = false;
+        while (!modeFound) {
+            System.out.print("Play casual mode or round mode? Type 'c' for casual or 'r' for round.\n> ");
+            String input = scan.nextLine();
+            if (input.equalsIgnoreCase("c")) {
+                modeFound = true;
+                System.out.println();
+                playCasualMode();
+            } else if (input.equalsIgnoreCase("r")) {
+                modeFound = true;
+            } else {
+                System.out.println("Not a valid answer.");
+            }
+        }
+
         boolean setFound = false;
         while (!setFound) {
             System.out.print("Enter set number from 1 to 8\n> ");
             String input = scan.nextLine();
             try {
                 set = Integer.parseInt(input);
-                if (set > 1 && set <= 8) {
-                    System.out.println("Sorry, that set of science bowl rounds does not exist yet!");
+                if (set < 1 || set > 8) {
+                    System.out.println("Sorry, that set of science bowl rounds does not exist.");
                 } else {
                     System.out.println("Set " + set + " chosen.");
                     setFound = true;
@@ -54,9 +76,7 @@ public class Proctor {
             String input = scan.nextLine();
             try {
                 round = Integer.parseInt(input);
-                if (round > 11 && round <= maxRound) {
-                    System.out.println("Sorry, that science bowl round does not exist yet!");
-                } else if (round > maxRound || round <= 0) {
+                if (round > maxRound || round <= 0) {
                     System.out.println("That is not a valid round number.");
                 } else {
                     System.out.println("Round " + round + " chosen.");
@@ -85,13 +105,14 @@ public class Proctor {
 
     private void readQuestions() throws IOException, InterruptedException {
         readRules();
-        String filepath = "Set" + set + "Round" + round + ".txt";
+        String filepath = ("set " + set + "\\Set" + set + "Round" + round + ".txt");
         File file = new File(filepath);
         BufferedReader br = new BufferedReader(new FileReader(file));
         Scanner user = new Scanner(System.in);
         boolean done = false;
         int score = 0;
         int qCount = 0;
+        boolean asked = false;
         while (!done) {
             boolean interrupt = false;
             stopwatch.reset();
@@ -107,24 +128,33 @@ public class Proctor {
                 int totalChars = 0;
                 String topic = br.readLine();
                 String answerType = br.readLine();
-                System.out.println(questionType.trim() + ": " + topic + " " + answerType);
                 String question = br.readLine();
                 totalChars = question.length();
-                System.out.println(question);
+                Question q = new Question(questionType, topic, answerType, question);
+                //System.out.println(questionType);
+                //System.out.println(topic);
+                //System.out.println(answerType);
+                //System.out.println(question);
                 if (answerType.equals("MULTIPLE CHOICE")) {
                     String w = br.readLine();
                     String x = br.readLine();
                     String y = br.readLine();
                     String z = br.readLine();
-                    System.out.println(w);
-                    System.out.println(x);
-                    System.out.println(y);
-                    System.out.println(z);
+                    q.setAnswerW(w);
+                    q.setAnswerY(y);
+                    q.setAnswerX(x);
+                    q.setAnswerZ(z);
+                    //System.out.println(w);
+                    //System.out.println(x);
+                    //System.out.println(y);
+                    //System.out.println(z);
                     totalChars+=w.length();
                     totalChars+=x.length();
                     totalChars+=y.length();
                     totalChars+=z.length();
                 }
+                q.printQuestion();
+                asked = false;
                 if (!fastMode) {
                     Thread.sleep((long) (totalChars * OFFSET_MULTIPLIER));
                     System.out.println("TIMER START");
@@ -156,6 +186,7 @@ public class Proctor {
                 String[] correctAnswers = answers.split(" OR ");
                 if (outOfTime) {
                     System.out.println("Your response was out of time.");
+                    System.out.println("Accepted answer(s): " + answers);
                     correct = false;
                 } else {
                     int wrongCount = 0;
@@ -191,7 +222,7 @@ public class Proctor {
             } else if (qCount == 25 && correct && questionType.equals("BONUS")) {
                 done = true;
             }
-            if (!fastMode && correct) {
+            if (!fastMode && !asked) {
                 System.out.print("Next question? Type 'y' for yes or 'n' for no\n> ");
                 String input = user.nextLine();
                 while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
@@ -202,6 +233,7 @@ public class Proctor {
                     done = true;
                 }
                 System.out.println();
+                asked = true;
             }
         }
         System.out.print("Final score: " + score + ". ");
@@ -262,5 +294,179 @@ public class Proctor {
             input = scan.nextLine();
         }
         System.out.println();
+    }
+
+    private void getAllQuestions() throws IOException {
+        for (int i = 1; i <= 2; i++) {
+            File folder = new File("set " + i);
+            File[] files = folder.listFiles();
+            for (File f : files) {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                int numLines = countLines(f.getAbsolutePath());
+                int j = 0;
+                while (j < numLines) {
+                    String questionType = br.readLine().trim();
+                    String topic = br.readLine().trim();
+                    String answerType = br.readLine().trim();
+                    String question = br.readLine().trim();
+                    j+=4;
+                    Question q = new Question(questionType, topic, answerType, question);
+                    if (answerType.equals("MULTIPLE CHOICE")) {
+                        String w = br.readLine().trim();
+                        String x = br.readLine().trim();
+                        String y = br.readLine().trim();
+                        String z = br.readLine().trim();
+                        j+=4;
+                        q.setAnswerW(w);
+                        q.setAnswerX(x);
+                        q.setAnswerY(y);
+                        q.setAnswerZ(z);
+                    }
+                    String[] answers = br.readLine().trim().split(" OR ");
+                    j++;
+                    q.addAnswers(answers);
+                    unread.add(q);
+                    br.readLine();
+                    j++;
+                }
+            }
+        }
+        for (int i = 1; i <= 6; i++) {
+            File f = new File("set 3\\Set3Round" + i + ".txt");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            int numLines = countLines(f.getAbsolutePath());
+            int j = 0;
+            while (j < numLines) {
+                String questionType = br.readLine().trim();
+                String topic = br.readLine().trim();
+                String answerType = br.readLine().trim();
+                String question = br.readLine().trim();
+                j+=4;
+                Question q = new Question(questionType, topic, answerType, question);
+                if (answerType.equals("MULTIPLE CHOICE")) {
+                    String w = br.readLine().trim();
+                    String x = br.readLine().trim();
+                    String y = br.readLine().trim();
+                    String z = br.readLine().trim();
+                    j+=4;
+                    q.setAnswerW(w);
+                    q.setAnswerX(x);
+                    q.setAnswerY(y);
+                    q.setAnswerZ(z);
+                }
+                String[] answers = br.readLine().trim().split(" OR ");
+                j++;
+                q.addAnswers(answers);
+                unread.add(q);
+                br.readLine();
+                j++;
+            }
+        }
+    }
+
+    private int countLines(String filename) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(filename));
+        try {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } finally {
+            is.close();
+        }
+    }
+
+    private void getAllByRound(int set, int round) throws IOException {
+        File f = new File("set " + set + "\\Set" + set + "Round" + round + ".txt");
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        int numLines = countLines("set " + set + "\\Set" + set + "Round" + round + ".txt");
+        int i = 0;
+        while (i < numLines) {
+            try {
+                String questionType = br.readLine().trim();
+                String topic = br.readLine().trim();
+                String answerType = br.readLine().trim();
+                String question = br.readLine().trim();
+                i += 4;
+                Question q = new Question(questionType, topic, answerType, question);
+                if (answerType.equals("MULTIPLE CHOICE")) {
+                    String w = br.readLine().trim();
+                    String x = br.readLine().trim();
+                    String y = br.readLine().trim();
+                    String z = br.readLine().trim();
+                    i += 4;
+                    q.setAnswerW(w);
+                    q.setAnswerX(x);
+                    q.setAnswerY(y);
+                    q.setAnswerZ(z);
+                }
+                try {
+                    String[] answers = br.readLine().trim().split(" OR ");
+                    i++;
+                    q.addAnswers(answers);
+                } catch (NullPointerException e) {
+                    System.out.println(question);
+                }
+                unread.add(q);
+            } catch (NullPointerException e) {
+                System.out.println(i);
+            }
+            br.readLine();
+            i++;
+        }
+    }
+
+    public void playCasualMode() throws IOException {
+        getAllQuestions();
+        //test(3, 10);
+        boolean done = false;
+        Random random = new Random();
+        Scanner user = new Scanner(System.in);
+        while (!done) {
+            int randQ = random.nextInt(unread.size());
+            Question q = unread.get(randQ);
+            q.printQuestionWithoutType();
+            String response = user.nextLine();
+            if (q.isCorrect(response)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Incorrect.");
+                System.out.print("Correct answers are: " + q.correctAnswers()+"\n");
+            }
+            System.out.print("Next question? Type 'y' for yes or 'n' for no\n> ");
+            String input = user.nextLine();
+            while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
+                System.out.print("Type 'y' for yes or 'n' for no\n> ");
+                input = user.nextLine();
+            }
+            if (input.equalsIgnoreCase("n")) {
+                done = true;
+            }
+            System.out.println();
+            unread.remove(randQ);
+            if (unread.size()==0) {
+                done = true;
+            }
+        }
+        System.out.println("Thanks for playing!");
+        System.exit(0);
+    }
+
+    private void test(int set, int round) throws IOException {
+        getAllByRound(set, round);
+        for (int i = 0; i < unread.size(); i++) {
+            Question q = unread.get(i);
+            q.showErrors((i+1));
+        }
+        System.exit(0);
     }
 }
---
+++ src/Question.java
@@ -0,0 +1,123 @@
+import org.apache.commons.lang3.text.WordUtils;
+
+import java.util.Arrays;
+
+/**
+ * Created by nishu on 5/14/2017.
+ */
+public class Question {
+    private String questionType;
+    private String topic;
+    private String answerType;
+    private String question;
+    private String[] answers;
+    private String answerW;
+    private String answerX;
+    private String answerY;
+    private String answerZ;
+    public Question(String questionType, String topic, String answerType, String question) {
+        this.questionType = questionType;
+        this.topic = topic;
+        this.answerType = answerType;
+        this.question = question;
+    }
+
+    public void addAnswers(String[] answers) {
+        this.answers = answers;
+    }
+
+    public void setAnswerW(String answerW) {
+        this.answerW = answerW;
+    }
+    public void setAnswerX(String answerX) {
+        this.answerX = answerX;
+    }
+    public void setAnswerY(String answerY) {
+        this.answerY = answerY;
+    }
+    public void setAnswerZ(String answerZ) {
+        this.answerZ = answerZ;
+    }
+
+    public void printQuestion() {
+        System.out.println(questionType);
+        System.out.println(topic);
+        System.out.println(answerType);
+        System.out.println(wrapQuestion(question));
+        if (answerW!=null)
+            System.out.println(answerW);
+        if (answerX!=null)
+            System.out.println(answerX);
+        if (answerY!=null)
+            System.out.println(answerY);
+        if (answerZ!=null)
+            System.out.println(answerZ);
+    }
+
+    public void printQuestionWithoutType() {
+        System.out.println(topic);
+        System.out.println(answerType);
+        System.out.println(wrapQuestion(question));
+        if (answerW!=null)
+            System.out.println(answerW);
+        if (answerX!=null)
+            System.out.println(answerX);
+        if (answerY!=null)
+            System.out.println(answerY);
+        if (answerZ!=null)
+            System.out.println(answerZ);
+    }
+
+    public boolean isCorrect(String answer) {
+        for (String a : answers) {
+            if (answer.equalsIgnoreCase(a.trim())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private String wrapQuestion(String q) {
+        return WordUtils.wrap(q, 120);
+    }
+
+    public void showErrors(int num) {
+        /*if (this.questionType.trim().length()==0 || this.topic.trim().length()==0 || this.answerType.trim().length()==0
+                || this.question.trim().length()==0) {
+            return true;
+        } else if (this.answerType.trim().equalsIgnoreCase("MULTIPLE CHOICE")) {
+            if (this.answerW.trim().length()==0 || this.answerX.trim().length()==0 || this.answerY.trim().length()==0 || this.answerZ.trim().length()==0) {
+                return true;
+            }
+        }
+        return false;*/
+        if (!questionType.equals("TOSS UP") && !questionType.equals("BONUS")) {
+            System.out.println("Question Type error on question " + num + "(question type = " + questionType + ")");
+        } else if (!topic.equals("BIOLOGY") && !topic.equals("CHEMISTRY") && !topic.equals("EARTH SCIENCE") && !topic.equals("PHYSICS") && !topic.equals("GENERAL SCIENCE") &&
+                !topic.equals("MATH") && !topic.equals("ENERGY") && !topic.equals("ASTRONOMY") && !topic.equals("COMPUTER SCIENCE")) {
+            System.out.println("Topic error on question " + num + "(topic = " + topic + ")");
+        } else if (!answerType.equals("SHORT ANSWER") && !answerType.equals("MULTIPLE CHOICE")) {
+            System.out.println("Answer type error on question " + num);
+        } else if (question.trim().length()==0) {
+            System.out.println("Question is blank error on question " + num);
+        } else if (answers.length==0) {
+            System.out.println("Answer is blank on question " + num);
+        }
+
+        if (answerType.equals("MULTIPLE CHOICE")) {
+            if (answerW.trim().length()==0) {
+                System.out.println("Choice W is blank error on question " + num);
+            } else if (answerX.trim().length()==0) {
+                System.out.println("Choice X is blank error on question " + num);
+            } else if (answerY.trim().length()==0) {
+                System.out.println("Choice Y is blank error on question " + num);
+            } else if (answerZ.trim().length()==0) {
+                System.out.println("Choice Z is blank error on question " + num);
+            }
+        }
+    }
+
+    public String correctAnswers() {
+        String line = Arrays.toString(this.answers);
+        return line.substring(1, line.length()-1);
+    }
+}
